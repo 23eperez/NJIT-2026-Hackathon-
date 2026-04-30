ac17b9c60f4018f6cd5f63a7c99ea6a8f88fc6b5
eduardo <ep3700299@gmail.com>
2026-04-29 23:48:04 -0400

    This will run the code of main.py with setting a new user to enter their name and school they go to

---
+++ main.py
@@ -0,0 +1,10 @@
+## This is a test run 
+
+
+
+print("Heello, WOrld")
+print("This is a test run for the main.py file")
+user_input = input("Please enter your name: ")
+print(f"Hello, {user_input}! Welcome to the test run of main.py")
+user_school = input("Please enter your school name: ")
+print(f"Great1! {user_school} is a wonderful school!")
