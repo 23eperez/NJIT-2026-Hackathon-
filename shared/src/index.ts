53b318736e11fdd77ae239f8bb8d9fb9ebd7a59b
eduardo <ep3700299@gmail.com>
2026-04-29 23:36:23 -0400

    This is the beginning of the backend

--- shared/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { Aircraft, Conflict, TrainingScenario, ControllerAction, ConflictDetectionRequest, ConflictDetectionResponse, PrebuiltScenario, ControllerInstruction } from './types.js';
-export { degreesToRadians, radiansToDegrees, calculateDistance, calculateVerticalSeparation, calculateTimeToClosestPoint, projectAircraftPosition } from './utils.js';
